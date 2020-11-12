0a4de145e782c1b22bc01a59ac90c662e9c82163
gioboa <giorgiob.boa@gmail.com>
2020-11-12 22:47:00 +0100

    dev: added topBar

--- lib/insta-home.dart
+++ lib/insta-home.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
 
 class InstaHome extends StatelessWidget {
+  final topBar = new AppBar(
+    backgroundColor: new Color(0xfff8faf8),
+    centerTitle: true,
+    elevation: 1.0,
+    leading: new Icon(Icons.camera_alt),
+    title: SizedBox(
+        height: 35.0, child: Image.asset("assets/images/insta_logo.png")),
+    actions: <Widget>[
+      Padding(
+        padding: const EdgeInsets.only(right: 12.0),
+        child: Icon(Icons.send),
+      )
+    ],
+  );
+
   @override
   Widget build(BuildContext context) {
-    return new Container();
+    return new Scaffold(
+      appBar: topBar,
+    );
   }
 }
